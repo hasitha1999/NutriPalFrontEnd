8d548b118b4074d261b5fdbfb122345f9b0dec74
hasitha <hasitha@convergence.lk>
2024-06-02 08:19:16 +0530

    forgot password

--- src/use-cases/login-user.js
+++ src/use-cases/login-user.js
@@ -1,3 +1,5 @@
 import axios from "axios";
 
 export const loginUser = (userData) => axios.post(process.env.REACT_APP_API_BASE_URL + '/auth/login', userData);
+
+export const getforgotPassword = (userData) => axios.post(process.env.REACT_APP_API_BASE_URL + '/auth/forgotPassword',userData);
