d1fd76240131761bd42667800543616772f4621b
NirmalHansaka <hansakanirmal123@gmail.com>
2024-06-27 19:16:15 +0530

    add line chart to dashboard

---
+++ src/component-ui/LineChartComponent/index.js
@@ -0,0 +1,20 @@
+import React, {useEffect, useState} from 'react'
+import { LineChart } from '@mui/x-charts/LineChart';
+
+const LineChartComponent = (props)=>{
+    return (
+        <LineChart
+            xAxis={[{ data: props.xAxisData }]}
+            series={[
+                {
+                    data: props.yAxisData,
+                    area: true,
+                },
+            ]}
+            width={500}
+            height={300}
+        />
+    );
+}
+
+export default LineChartComponent;
